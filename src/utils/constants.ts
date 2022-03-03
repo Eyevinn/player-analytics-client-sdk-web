e4bb656a58f20c47fd2faa5ebe2c3a419cd17a82
birme <jonas.birme@eyevinn.se>
2022-03-03 15:37:35 +0100

    fix: a more safe way of including epas package version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -18,5 +18,5 @@ export enum EPASEvents {
 
 export const HEARTBEAT_INTERVAL = 30000;
 
-const { version } = require("../../node_modules/@eyevinn/player-analytics-specification/package.json");
-export const EPAS_VERSION = version;
+import packageJson from "@eyevinn/player-analytics-specification/package.json";
+export const EPAS_VERSION = packageJson.version;
