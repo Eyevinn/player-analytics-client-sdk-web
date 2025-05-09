ec5678d15da69ae0fad8c4e160c22080804b591c
Ramanjot Kaur <79700214+iamramanjot@users.noreply.github.com>
2025-05-09 08:26:47 +0200

    Fix build process

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,6 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+class RemoveModuleScriptPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('RemoveModuleScriptPlugin', (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        'RemoveModuleScriptPlugin',
+        (data, cb) => {
+          // Remove the original script tag for demo.js
+          data.html = data.html.replace(
+            /<script type="module" src="demo.js"><\/script>/g,
+            ''
+          );
+          cb(null, data);
+        }
+      );
+    });
+  }
+}
 
 module.exports = {
   mode: 'production',
@@ -11,7 +31,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    clean: true
+    clean: true,
   },
   optimization: {
     minimize: true,
@@ -19,7 +39,6 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Eyevinn Open Analytics',
       template: './index.html',
       inject: 'body',
       minify: {
@@ -31,7 +50,26 @@ module.exports = {
         useShortDoctype: true
       }
     }),
-    new HtmlInlineScriptPlugin()
+    new RemoveModuleScriptPlugin(),
+    new HtmlInlineScriptPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        // Copy image files
+        {
+          from: '*.png',
+          to: '[name][ext]'
+        },
+        // Copy CSS files
+        {
+          from: 'style.css',
+          to: 'style.css',
+          noErrorOnMissing: true
+        }
+      ]
+    })
   ],
   module: {
     rules: [
@@ -43,6 +81,20 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env']
           }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]'
         }
       }
     ]
